a510331ca475b089fad231c87b1aaaa62b0ca7cc
r0b3rt[0] <nullepart@rien.com>
2021-07-18 13:03:53 +0200

    42

--- js.js
+++ js.js
@@ -1,22 +1,61 @@
 const MQ = MathQuill.getInterface(2);
 
-function MathLineInput(pEl) {
-    this.jQEl = pEl;
+function MathLineInput() {
+    this.jQEl = $('<p class="mathLineInput"></p>');
+    this.nextMathLineInput = null;
     this.mathField = MQ.MathField(this.jQEl[0], {
         handlers: {
             edit: () => {
-                
             },
     
             enter: () => {
-                
-            }
+                if (!(this.hasNextMathLineInput())) {
+                    let newMathLineInput = new MathLineInput();
+                        newMathLineInput.insertAfter(this.jQEl);
+
+                    this.nextMathLineInput = newMathLineInput;
+                    
+                }
+
+                this.nextMathLineInput.focus();
+                console.log('ok');
+            },
+
+            upOutOf: () => {
+                console.log('up');
+            },
+
+            downOutOf: () => {
+                console.log('down');
+            },
         }
     });
 
     this.undoRedoManager = new UndoRedoManager(this.mathField, this.jQEl);
+    
+    this.appendTo = function (pElement) {
+        this.jQEl.appendTo(pElement);
+    }
+
+    this.hasNextMathLineInput = () => (this.nextMathLineInput !== null);
+    this.insertAfter = (pElement) => {this.jQEl.insertAfter(pElement)};
+    this.focus = () => (this.mathField.focus());
 }
 
 //to translate the given textNode into proper math
-MQ.StaticMath(window.document.getElementById('problem'));
-let mathLineInput = new MathLineInput($('#content'));
+//MQ.StaticMath(window.document.getElementById('problem'));
+
+mathLineInputArray = [];
+for (let i = 0; i < 5; i++) {
+    mathLineInputArray[i] = new MathLineInput();
+    mathLineInputArray[i].appendTo($('#content'));
+
+    if (i !== 0) {
+        mathLineInputArray[i - 1].nextMathLineInput = mathLineInputArray[i];
+    }
+}
+
+console.log(mathLineInputArray[3].hasNextMathLineInput());
+console.log(mathLineInputArray[4].hasNextMathLineInput());
+
+mathLineInputArray[2].jQEl.focus();
